c1b36066b7580938593e39808703e69924d42ca9
hc_hero <zhou4xin3xin3520@163.com>
2020-11-23 12:16:31 +0800

    【element】1、使用element

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/index'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+
+
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/register',
+      name: 'Register',
+      component: Register
+    },
+    {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
-    }
+    },
   ]
 })
